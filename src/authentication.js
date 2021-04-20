a0832354369878bb71426634c87e3a432201a342
Ciro Santilli <ciro.santilli@gmail.com>
2021-04-20 20:07:24 +0100

    give the fuck up

--- src/authentication.js
+++ src/authentication.js
@@ -1,4 +1,5 @@
-const { AuthenticationService, JWTStrategy } = require('@feathersjs/authentication');
+const { AuthenticationService, JWTStrategy, authenticate } = require('@feathersjs/authentication');
+const authentication = require('@feathersjs/authentication');
 const { LocalStrategy } = require('@feathersjs/authentication-local');
 const { expressOauth, OAuthStrategy } = require('@feathersjs/authentication-oauth');
 
@@ -25,6 +26,44 @@ module.exports = app => {
   authentication.register('local', new LocalStrategy());
   authentication.register('github', new GitHubStrategy());
 
-  app.use('/authentication', authentication);
+  app.use('/users/login', authentication);
   app.configure(expressOauth());
+  //app.service('users/login').hooks({
+  //  before: {
+  //    create: [
+  //      authenticate(),
+  //    ],
+  //  },
+  //  after: {
+  //    create: [
+  //      authenticateResponse(),
+  //    ],
+  //  },
+  //});
+  app.service('users/login').hooks({
+    before: {
+      create: [
+        // The frontend does not send 'strategy' as per realworld spec.
+        // Feathers expects {email: password: }, but the RealWorld spec
+        // sends {user:{email: password:}}
+        context => {
+          context.data = {
+            ...context.data.user,
+            strategy: 'local',
+          };
+          return context;
+        },
+      ],
+    },
+    after: {
+      create: [
+        // Realworld frontend expects {user.token} and FeathersJS
+        // produces {accessToken}.
+        context => {
+          context.result.user.token = context.result.accessToken;
+          return context;
+        }
+      ],
+    },
+  });
 };
